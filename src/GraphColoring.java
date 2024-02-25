14a130cb8aa2ab02b3898b5fb54aec1a8b880210
zerusfree <115157823+zerusfree@users.noreply.github.com>
2024-02-25 12:57:42 +0200

    Initial commit

---
+++ src/GraphColoring.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+public class GraphColoring {
+    private int V;
+    private List<Integer> adj[];
+
+    GraphColoring(int v) {
+        V = v;
+        adj = new ArrayList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new ArrayList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+    }
+
+    void greedyColoring() {
+        int result[] = new int[V];
+
+        Arrays.fill(result, -1);
+
+        result[0]  = 0;
+
+        boolean available[] = new boolean[V];
+        Arrays.fill(available, true);
+
+        for (int u = 1; u < V; u++) {
+            Iterator<Integer> it = adj[u].iterator() ;
+            while (it.hasNext()) {
+                int i = it.next();
+                if (result[i] != -1)
+                    available[result[i]] = false;
+            }
+
+            int cr;
+            for (cr = 0; cr < V; cr++)
+                if (available[cr])
+                    break;
+
+            result[u] = cr;
+
+            Arrays.fill(available, true);
+        }
+
+        for (int u = 0; u < V; u++)
+            System.out.println("Vertex " + u + " --->  Color "
+                    + result[u]);
+    }
+
+    public static void main(String args[]) {
+        GraphColoring g1 = new GraphColoring(5);
+        g1.addEdge(0, 1);
+        g1.addEdge(0, 2);
+        g1.addEdge(1, 2);
+        g1.addEdge(1, 3);
+        g1.addEdge(2, 3);
+        g1.addEdge(3, 4);
+        System.out.println("Coloring of graph 1");
+        g1.greedyColoring();
+    }
+}
